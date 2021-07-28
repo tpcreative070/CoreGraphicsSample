ea010ab9254119289afb975f246417ef97bf77c8
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2021-07-28 16:55:35 +0700

    [mater] - The first commit

--- CoreGraphicsSample/ViewController.swift
+++ CoreGraphicsSample/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let drawRectView = MyView()
+    let imageView = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
         drawRectView.translatesAutoresizingMaskIntoConstraints = false
@@ -21,6 +22,28 @@ class ViewController: UIViewController {
                    drawRectView.heightAnchor.constraint(equalToConstant: 400),
         ])
         print(UIScreen.main.bounds.size)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: drawRectView.bottomAnchor,constant: 10)
+        ])
+        drawCircle()
     }
+    
+    func drawCircle() {
+           let renderer = UIGraphicsImageRenderer(size: CGSize(width: 200, height: 200))
+           let img = renderer.image { ctx in
+               let rectangle = CGRect(x: 0, y: 0, width: 200, height: 200).insetBy(dx: 5, dy: 5)
+               ctx.cgContext.setFillColor(UIColor.red.cgColor)
+               ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+               ctx.cgContext.setLineWidth(10)
+
+               ctx.cgContext.addEllipse(in: rectangle)
+               ctx.cgContext.drawPath(using: .fillStroke)
+           }
+           imageView.image = img
+       }
 }
 
