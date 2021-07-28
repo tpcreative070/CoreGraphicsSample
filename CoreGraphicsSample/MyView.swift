70c07cf404f778488d791a9c7a2226d8852769d2
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2021-07-28 16:34:47 +0700

    [mater] - The first commit

---
+++ CoreGraphicsSample/MyView.swift
@@ -0,0 +1,40 @@
+//
+//  MyView.swift
+//  CoreGraphicsSample
+//
+//  Created by phong070 on 28/07/2021.
+//
+
+import UIKit
+class MyView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+
+        let rectangle1 = CGRect(x: 0, y: 0, width: 200, height: 180).insetBy(dx: 10, dy: 10)
+                
+        context.setFillColor(UIColor.systemRed.cgColor)
+        context.setStrokeColor(UIColor.systemGreen.cgColor)
+        context.setLineWidth(20)
+        context.addRect(rectangle1)
+        context.drawPath(using: .fillStroke)
+        context.fill(rectangle1)
+        
+        let rectangle2 = CGRect(x: 256, y: 256, width: 128, height: 128)
+
+        context.setFillColor(UIColor.systemYellow.cgColor)
+        context.setStrokeColor(UIColor.systemBlue.cgColor)
+        context.setLineWidth(10)
+        context.addEllipse(in: rectangle2)
+        context.drawPath(using: .fillStroke)
+    }
+}
--- CoreGraphicsSample/ViewController.swift
+++ CoreGraphicsSample/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let drawRectView = MyView()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        drawRectView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(drawRectView)
+        NSLayoutConstraint.activate([
+                   drawRectView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   drawRectView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                   drawRectView.widthAnchor.constraint(equalToConstant: 400),
+                   drawRectView.heightAnchor.constraint(equalToConstant: 400),
+        ])
+        print(UIScreen.main.bounds.size)
     }
-
-
 }
 
